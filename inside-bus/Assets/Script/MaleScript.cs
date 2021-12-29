a40970e7a14ab734d1878624e86cc936795c0208
Gianluca Farinaccio <gianluca.farinaccio@gmail.com>
2021-12-29 22:44:14 +0100

    refactoring di MaleScript.cs
    purtroppo il passaggio di valori tramite variabili private ancora non funziona. per ora ok solo se la variabili sono public

--- inside-bus/Assets/Script/MaleScript.cs
+++ inside-bus/Assets/Script/MaleScript.cs
@@ -4,14 +4,43 @@ using UnityEngine;
 
 public class MaleScript : MonoBehaviour
 {
+    
+    // parameters for switch the animation of associated game object 
+    public float HorizontalValue; // value for horizontal axis direction of animation  ; range [-1;1] ; 
+    public float VerticalValue; // value for vertical axis direction of animation  ; range [-1;1] ; 
+    public float AnimationSpeed; // value for animation speed  ; range [0; ...] ; 
+                                          //if this <= 0.01 --> animation in IDLE position 
+    
+    // some components associated to game object...
+    public Rigidbody2D RigidBody; 
+    public Animator Animator; 
 
-	public float hValue;
-	public float vValue;
-	public float moveSpeed;
 
-	public Rigidbody2D rb;
-	public Animator Animator;
-	Vector2 movement;
+    // setters and getters 
+    public void SetHorizontalValue(float value){ 
+        HorizontalValue = value;
+    }
+
+    public float GetHorizontalValue(){
+        return HorizontalValue;
+    }
+
+    public void SetVerticalValue(float value){ 
+        VerticalValue = value;
+    }
+
+    public float GetVerticalValue(){
+        return VerticalValue;
+    }
+
+    public void SetAnimationSpeed(float value){ 
+        AnimationSpeed = value;
+    }
+
+    public float GetAnimationSpeed(){
+        return AnimationSpeed;
+    }
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,17 +51,15 @@ public class MaleScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Input
+        // if we want to switch the male animation from the keyboard or other input...
         //movement.x = Input.GetAxisRaw("Horizontal"); // return int from -1 to 1
         //movement.y = Input.GetAxisRaw("Vertical"); // return int from -1 to 1
         
-        Animator.SetFloat("Horizontal", hValue);
-        Animator.SetFloat("Vertical", vValue);
-        Animator.SetFloat("Speed", moveSpeed);
+        // Updating the parameters of animation...
+        Animator.SetFloat("Horizontal", HorizontalValue);
+        Animator.SetFloat("Vertical", VerticalValue);
+        Animator.SetFloat("Speed", AnimationSpeed);
   
-    }
-
-    void FixedUpdate(){
     }
 
 }
