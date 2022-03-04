68289886882e4ee789db1f6f8434f74836ecd38d
Matteo <mat.cardilli@stud.uniroma3.it>
2022-03-04 16:32:31 +0100

    prima verifica risposta utente e bozza animazioni bus

--- inside-bus/Assets/Script/MainScript.cs
+++ inside-bus/Assets/Script/MainScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,14 +9,24 @@ public class MainScript : MonoBehaviour
     public TimeScript timer;
     public AnswerScript answer;
     public CharacterScript character;
+    public Text lives;
     private float delay = 0;
     private bool flag = false;
     private int rispostaEsatta;
 
+    public int getRispostaEsatta()
+    {
+        return rispostaEsatta;
+    }
+
+    public Text getLives()
+    {
+        return lives;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        //rispostaEsatta = 120;
+        rispostaEsatta = 120;
     }
 
     // Update is called once per frame
@@ -23,39 +34,22 @@ public class MainScript : MonoBehaviour
     {
 
 
-        
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
-        if (delay > 3 && (flag==false)){
+
+        if (delay > 3 && (flag == false)) {
             flag = true;
             character.Spawn("model1", 5, 1f);
             Debug.Log("inviato " + delay);
         }
-        Debug.Log(delay);
+
+        //Debug.Log(delay);
         delay += Time.fixedDeltaTime;
-
-        //if (VerificaRisposta())
-            //Debug.Log("hai vinto");
-
-    }
-
-    public bool VerificaRisposta()
-    {
-        float numero = timer.timerValue;
-        if (numero == 0.0f && answer.moreButton.interactable == false)
-        {
-            if (answer.AnswerValue == rispostaEsatta)
-                return true;
-            else
-                return false;
-        }
-        else
-            return false;
-
+        
     }
 
 }
