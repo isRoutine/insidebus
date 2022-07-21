83d28355074ae6b2bbfae926e20a991ee47d411a
Alexis <osvaldomar144@gmail.com>
2022-07-21 16:56:40 +0200

    Added Login and register functions

--- inside-bus/Assets/Script/PlayFabManager.cs
+++ inside-bus/Assets/Script/PlayFabManager.cs
@@ -3,30 +3,119 @@ using System.Collections.Generic;
 using UnityEngine;
 using PlayFab;
 using PlayFab.ClientModels;
+using TMPro;
 
 public class PlayFabManager : MonoBehaviour
 {
+
+    /* login and register */
+    [Header("UI")]
+    public TMP_Text messageText;
+    public TMP_InputField usernameInput;
+    public TMP_InputField emailInput;
+    public TMP_InputField passwordInput;
+    public TMP_InputField confirmPasswordInput;
+
+    [Header("SCREEN")]
+    [SerializeField]
+    private GameObject loginUI;
+    [SerializeField]
+    private GameObject registerUI;
+
+    public void RegisterButton(){
+
+        if(passwordInput.text.Length < 6) {
+            messageText.text = "Password troppo corta!";
+            return;
+        }
+
+        if(!(passwordInput.text).Equals(confirmPasswordInput.text)){
+            messageText.text = "Le Password inserite non coincidono!";
+            return;
+        }
+
+        var request = new RegisterPlayFabUserRequest(){
+            Username = usernameInput.text,
+            Email = emailInput.text,
+            Password = passwordInput.text,
+            RequireBothUsernameAndEmail = true
+        };
+        PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnError);
+    }
+
+    void OnRegisterSuccess(RegisterPlayFabUserResult result){
+        messageText.text = "Registered and logged in!";
+        Debug.Log("Registered and logged in!");
+        /* posso passare alla schermata di gioco o profilo */
+    }
+
+    public void LoginButton(){
+        var request = new LoginWithEmailAddressRequest {
+            Email = emailInput.text,
+            Password = passwordInput.text
+        };
+
+        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnError);
+    }
+
+
+    void OnLoginSuccess(LoginResult result){
+        messageText.text = "Logged in!";
+        Debug.Log("Successfull login/account create!");
+        /* posso passare alla schermata di gioco */
+    }
+
+    public void ResetPasswordButton(){
+
+    }
+
+    void OnPasswordReset (SendAccountRecoveryEmailResult result){
+
+    } 
+
+    /* UI */
+    public void ClearUI()
+    {
+        loginUI.SetActive(false);
+        registerUI.SetActive(false);
+        messageText.text = "";
+    }
+
+    public void LoginScreen()
+    {
+        ClearUI();
+        loginUI.SetActive(true);
+    }
+
+    public void RegisterScreen()
+    {
+        ClearUI();
+        registerUI.SetActive(true);
+    }
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        Login();
+        //Login();
     }
+
+    /* LOGIN ANONIMO - guest
 
     void Login(){
         var request = new LoginWithCustomIDRequest {
             CustomId = SystemInfo.deviceUniqueIdentifier,
             CreateAccount = true
         };
-        PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
-    }
-
-    void OnSuccess(LoginResult result){
-        Debug.Log("Successfull login/account create!");
-    }
+        PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnError);
+    }*/
 
     void OnError(PlayFabError error){
         Debug.Log("Error while login/creating account!");
         Debug.Log(error.GenerateErrorReport());
+        messageText.text = error.ErrorMessage;
     }
 
     /* funzioni per la classifica */
